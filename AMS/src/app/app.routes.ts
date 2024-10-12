1745b1e1fe6699a503174680e207d802f1cbd1d6
Jirro Aeron Guiao <79578145+kkazei@users.noreply.github.com>
2024-10-12 09:50:01 +0800

    sp

--- AMS/src/app/app.routes.ts
+++ AMS/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { TenantDashboardComponent } from './tenant-dashboard/tenant-dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'admin-dashboard', component: AdminDashboardComponent },
   { path: 'tenant-dashboard', component: TenantDashboardComponent },
  
 
