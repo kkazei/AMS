3c06259db8868b3069c563a97ee911b2763ca7bf
PickleC137 <nlfuenticilla@gmail.com>
2024-11-26 14:00:31 +0800

    dashboard for the billing system

--- AMS/src/app/tenant-dashboard/tenant-dashboard.component.ts
+++ AMS/src/app/tenant-dashboard/tenant-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-tenant-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './tenant-dashboard.component.html',
   styleUrl: './tenant-dashboard.component.css'
 })
