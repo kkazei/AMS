d7807fa92c507acdd6802515b5769cf05fe107bd
Jirro Aeron Guiao <79578145+kkazei@users.noreply.github.com>
2024-12-01 20:03:59 +0800

    sidenav

--- AMS/src/app/sidenav/sidenav.component.ts
+++ AMS/src/app/sidenav/sidenav.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../services/auth.services';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
   isCollapsed = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: (response) => {
+        console.log('Logged out successfully:', response);
+        this.router.navigate(['/login']); // Redirect to login or any other page
+      },
+      error: (error) => {
+        console.error('Logout error:', error);
+        // Optionally display an error message to the user
+      },
+    });
+  }
+
 
   toggleSidenav() {
     this.isCollapsed = !this.isCollapsed;
