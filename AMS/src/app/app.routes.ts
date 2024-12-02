40b89b43a533d35306ce6889a00986e4be27dc77
PickleC137 <nlfuenticilla@gmail.com>
2024-12-02 18:29:29 +0800

    drop email

--- AMS/src/app/app.routes.ts
+++ AMS/src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login/login.component';
 import { LandlordDashboardComponent } from './landlord-dashboard/landlord-dashboard.component';
 import { TenantManagementComponent } from './tenant-management/tenant-management.component';
 import { LandlordApartmentComponent } from './landlord-apartment/landlord-apartment.component';
-import { ComposeMailComponent } from './compose-mail/compose-mail.component';
 import { TenantDashboardComponent } from './tenant-dashboard/tenant-dashboard.component';
 import { RegisterComponent } from './register/register.component';
 import { TenantLoginComponent } from './tenant-login/tenant-login.component';
@@ -22,7 +21,6 @@ export const routes: Routes = [
   { path: 'landlord-dashboard', component: LandlordDashboardComponent },
   { path: 'tenant-management', component: TenantManagementComponent },
   { path: 'landlord-apartment', component: LandlordApartmentComponent },
-  { path: 'compose-mail', component: ComposeMailComponent },
   { path: 'tenant-dashboard', component: TenantDashboardComponent },
   {path: 'register', component: RegisterComponent},
   {path: 'tenant-login', component: TenantLoginComponent},
